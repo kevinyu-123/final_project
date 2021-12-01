1b47d5e0036649afe445a6a71d29b3c2fc3a7a8a
yhj <tntj123@naver.com>
2021-12-01 21:27:12 +0900

    dininglab setting

---
+++ src/main/java/com/dine/root/common/session/MemberSession.java
@@ -0,0 +1,5 @@
+package com.dine.root.common.session;
+
+public interface MemberSession {
+	static final String LOGIN_ID = "session_user";
+}
---
+++ src/main/java/com/dine/root/config/MailConfig.java
@@ -0,0 +1,30 @@
+package com.dine.root.config;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+	   @Bean
+	   public static JavaMailSender mailSender() {
+	   JavaMailSenderImpl jms = new JavaMailSenderImpl();
+	   jms.setHost("smtp.gmail.com");
+	   jms.setPort(587);
+	   jms.setUsername("dininglab.korea@gmail.com");
+	   jms.setPassword("dbguswns123");
+	   
+	   Properties prop = new Properties();
+	   prop.setProperty("mail.transport.protocol", "smtp");
+	   prop.setProperty("mail.smtp.auth", "true"); 
+	   prop.setProperty("mail.smtp.starttls.enable", "true");
+	   
+	   prop.setProperty("mail.debug", "true");
+	   jms.setJavaMailProperties(prop);
+	   
+	   return jms;
+	    }
+}
