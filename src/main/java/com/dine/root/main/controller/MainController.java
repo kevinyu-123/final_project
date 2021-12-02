c58e0b391bfff6d075145e5ac5682a600d5fee45
eun <eb3020@naver.com>
2021-12-02 20:35:06 +0900

    main컨트롤러만추가test

---
+++ src/main/java/com/dine/root/main/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.dine.root.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("main")
+	public String main() {
+		return "main/mainPage";
+	}
+
+
+}
